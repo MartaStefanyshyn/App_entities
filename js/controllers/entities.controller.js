dd2818d21d606b9ce5b8833e7a896d66ad2260be
Marta Stefanyshyn <martaolehivna@gmail.com>
2016-01-05 11:57:24 +0200

    http get JSON data from file

--- js/controllers/entities.controller.js
+++ js/controllers/entities.controller.js
@@ -6,7 +6,7 @@
     .controller('EntitiesController', EntitiesController);
 
   /** @ngInject */
-  function EntitiesController($location, $uibModal, $log, $rootScope) {
+  function EntitiesController($location, $uibModal, $log, $rootScope, $http) {
     var vm = this;
     vm.entits = [];
     for (var i = 0; i<localStorage.length; i++) {
@@ -16,12 +16,26 @@
       vm.entits.push(JSON.parse(localStorage.getItem(obj.title)));
     });
 
+    vm.retrieveFromFile = function(){
+      $http.get('/entities.json')
+        .then(function(result) {
+          angular.forEach(result.data, function(entity) {
+            if (!JSON.parse(localStorage.getItem(entity.title))){
+              localStorage.setItem(entity.title, JSON.stringify(entity));
+              vm.entits.push(JSON.parse(localStorage.getItem(entity.title)));
+            }
+          });
+      });
+    };
+    vm.clearStorage = function(){
+      localStorage.clear();
+      vm.entits = [];
+    };
     vm.addEntity = function(){
       localStorage.setItem(vm.entit.title, JSON.stringify(vm.entit));
       vm.entits.push(JSON.parse(localStorage.getItem(vm.entit.title)));
       vm.entit = {};
     };
-
     vm.deleteEntity = function(item){
       localStorage.removeItem(item)
       vm.entits.splice(JSON.parse(localStorage.getItem(item)), 1);
